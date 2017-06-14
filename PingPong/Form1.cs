d3a4b493d0f1d456f4207f7f07d06f90af487abf
sador23 <adorjns@gmail.com>
2017-06-14 13:27:10 +0200

    space to move in extended

--- PingPong/Form1.cs
+++ PingPong/Form1.cs
@@ -10,27 +10,18 @@ using System.Windows.Forms;
 
 namespace PingPong
 {
-
-    
-
     public partial class Form1 : Form
     {
-
         private Ball ball;
         private Player player;
-        private Rectangle circle;
         private bool isStopped;
-
         System.Windows.Forms.Timer t;
-        private int x=20;
-        private int y=20;
 
         private void Timer_Shown(object sender, EventArgs e)
         {
             t = new System.Windows.Forms.Timer();
             t.Interval = 20;
             t.Tick += new EventHandler(t_Tick);
-        
             t.Start();
         }
         
@@ -40,8 +31,6 @@ namespace PingPong
             score.Text = this.player.GetScore().ToString();
             if (!isStopped)
             {
-
-            
             this.ball.move();
                 if(ball.IsOutOfGame())
                 {
@@ -49,11 +38,9 @@ namespace PingPong
                     this.Close();
                 }
             this.ball.CheckCollide(progressBar1, player);
-                if (ball.GetCurrent() == ball.GetDiff()) this.Close();
+            if (ball.GetCurrent() == ball.GetDiff()) this.Close();
             Invalidate();
             }
-            //if (progressBar.Value >= 100) t.Stop();
-
         }
 
         public Form1(Ball ball, Player player, int level)
@@ -62,11 +49,9 @@ namespace PingPong
             this.player = player;
             this.KeyPreview = true;
             this.isStopped = false;
-            
             InitializeComponent();
             username.Text = this.player.GetUsername();
             this.level.Text = level.ToString();
-
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -77,31 +62,25 @@ namespace PingPong
                 this.Close();
             }
             if (e.KeyData == Keys.Space) isStopped = !isStopped;
-
             if (!isStopped)
             {
-
-            
-
-            if (progressBar1.Location.X > 1 && progressBar1.Location.X < (this.Size.Width/2))
-            {
-            if(e.KeyData == Keys.Left)
-            {
-                progressBar1.Location = new Point(progressBar1.Location.X -5, progressBar1.Location.Y);
+                if (progressBar1.Location.X > 1 && progressBar1.Location.X < (this.Size.Width/2+25))
+                {
+                    if(e.KeyData == Keys.Left)
+                    {
+                        progressBar1.Location = new Point(progressBar1.Location.X -5, progressBar1.Location.Y);
+                    }
+                    else if (e.KeyData == Keys.Right)
+                    {
+                        progressBar1.Location = new Point(progressBar1.Location.X +5, progressBar1.Location.Y);
+                    }
+                }
+                else
+                {
+                    if (progressBar1.Location.X < 2) progressBar1.Location = new Point(2, progressBar1.Location.Y);
+                    else progressBar1.Location = new Point(173, progressBar1.Location.Y);
+                }
             }
-            else if (e.KeyData == Keys.Right)
-            {
-                progressBar1.Location = new Point(progressBar1.Location.X +5, progressBar1.Location.Y);
-            }
-            }
-            else
-            {
-                if (progressBar1.Location.X < 2) progressBar1.Location = new Point(2, progressBar1.Location.Y);
-                else progressBar1.Location = new Point(130, progressBar1.Location.Y);
-            }
-            }
-
-
         }
 
         void Form1_Load(object sender, System.EventArgs e)
@@ -116,7 +95,6 @@ namespace PingPong
             Pen p = new Pen(Color.Red);
             g.DrawEllipse(p, this.ball.GetBall());
             g.Dispose();
-           // Invalidate();
         }
 
         private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
